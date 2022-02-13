fbca2390077749e8bcd13b3f065c820a34968389
sonia2605 <87336226+sonia2605@users.noreply.github.com>
2022-02-13 16:20:29 +0100

    code rangé

--- server.js
+++ server.js
@@ -1,52 +1,53 @@
 // données transmises en clair
-const http = require('http');
+const http = require("http");
 // exportation express
-const app = require ('./app');
+const app = require("./app");
 
 // normalizePort renvoie un code valide qu'il soit fournit en number ou string
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
-  };
-  const port = normalizePort(process.env.PORT || '3000');
-  app.set('port', port);
-  
-  // errorHandler gere les erreurs et les enregistre dans le serveur
-  const errorHandler = error => {
-    if (error.syscall !== 'listen') {
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// errorHandler gere les erreurs et les enregistre dans le serveur
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
+  }
+};
 
 // fonction exécutée pour chaque requête au serveur avec notre fonction app
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
+server.on("error", errorHandler);
+server.on("listening", () => {
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
 server.listen(port);
